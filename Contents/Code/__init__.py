cff7ddd3f052b7080e339772373e0e59438dfbe5
sander1 <sanderspies+plexapp@gmail.com>
2010-12-19 03:06:20 +0100

    Display a message when a channel is offline

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -9,22 +9,31 @@ def Start():
   Plugin.AddPrefixHandler('/video/tvcatchup', MainMenu, 'TVCatchup', 'icon-default.png', 'art-default.jpg')
   Plugin.AddViewGroup('InfoList', viewMode='InfoList', mediaType='items')
   MediaContainer.art = R('art-default.jpg')
-  HTTP.Headers['User-Agent'] = 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.6; en-US; rv:1.9.2.12) Gecko/20101026 Firefox/3.6.12'
+  HTTP.Headers['User-Agent'] = 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.6; en-US; rv:1.9.2.13) Gecko/20101203 Firefox/3.6.13'
 
 ####################################################################################################
 
 def MainMenu():
   dir = MediaContainer(viewGroup='InfoList', title1='TVCatchup Channel List')
 
-  channels = HTML.ElementFromURL(CHANNELS_URL, errors='ignore').xpath('//ul[@id="logos"]/li/a')
+  channels = HTML.ElementFromURL(CHANNELS_URL, errors='ignore', cacheTime=120).xpath('//ul[@id="logos"]/li/a')
   for channel in channels:
     title = channel.get('title').strip()
-    url = BASE_URL + channel.get('href')
     id = channel.xpath('./parent::li')[0].get('id').rsplit('_', 1)[1]
-    dir.Append(WebVideoItem(url, title=title, thumb=Function(GetThumb, channel_id=id)))
+    url = channel.get('href')
+    if url.find('javascript') == -1:
+      url = BASE_URL + url
+      dir.Append(WebVideoItem(url, title=title, thumb=Function(GetThumb, channel_id=id)))
+    else:
+      dir.Append(Function(DirectoryItem(ChannelIsOffline, title=title, thumb=Function(GetThumb, channel_id=id)), title=title))
 
   dir.Append(PrefsItem('Settings', thumb=R('icon-prefs.png')))
   return dir
+
+####################################################################################################
+
+def ChannelIsOffline(sender, title):
+  return MessageContainer('Channel Is Offline', title + ' Is Currently Offline')
 
 ####################################################################################################
 
